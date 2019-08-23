8ff0d73eabf3fb94fa5be6d9357f1e84dfd68964
Matt D <dwinell.matt@gmail.com>
2019-08-23 14:03:03 -0400

    added .gitignore and created userprofiles sequelize model

---
+++ controllers/html-routes.js
@@ -0,0 +1,21 @@
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/assets/html/login.html"));
+  });
+
+  // Route to the cms page
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/assets/html/login.html"));
+  });
+
+//   app.get("/assets/html/maker", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/assets/html/maker.html"));
+//   });
+
+};
