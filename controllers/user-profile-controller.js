76d2ce4051424765a4e32ceec2e89c2d39b25bd6
Matt D <dwinell.matt@gmail.com>
2019-08-23 14:20:36 -0400

    server runs and serves login page

--- controllers/user-profile-controller.js
+++ controllers/user-profile-controller.js
@@ -1,4 +1,4 @@
-var userProfiles = require("../models/character.js");
+var userProfiles = require("../models/user-profiles.js");
 module.exports = function(app){
     app.get("/api/user-profiles/:user", (req, res) => {
         const {user} = req.params;
