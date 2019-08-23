04cfeeb67adfcf71ab16d3a8feaed96a44765dd2
Matt D <dwinell.matt@gmail.com>
2019-08-23 13:40:10 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Plantr
-full stack web app for matching gardeners to gardens (solo project)
+full stack web app for matching gardeners to gardens.
+Bridging the gap between those with gardens and no time, and those who aspire to garden, but have not the space.
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var PORT = process.env.PORT || 8080;
+var app = express();
+var path = require('path');
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+require("./controllers/user-profile-controller.js")(app);
+require("./controllers/html-routes")(app);
+app.listen(PORT, function(){
+    console.log('current port:' + PORT);
+})
