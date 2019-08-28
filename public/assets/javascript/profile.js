684539be74578a7ce4296e8bd04f1b7ba58f8f68
Matt D <dwinell.matt@gmail.com>
2019-08-28 14:05:41 -0400

    working on making profile page editable

--- public/assets/javascript/profile.js
+++ public/assets/javascript/profile.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     let userEmail = window.localStorage.getItem('user-email');
+    var userObject;
     console.log(userEmail);
     profileCall();
 
@@ -12,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     let userArray = JSON.parse(xhr.response);
                     console.log(userArray);
                     console.log(userArray.length);
+                    userObject = userArray[0];
                     profilePopulate(userArray[0]);
                 } else {
                     errorLoadingProfile();
@@ -143,14 +145,80 @@ document.addEventListener("DOMContentLoaded", () => {
         let hoursWrapper = document.getElementById('hours');
         while (hoursWrapper.firstChild) hoursWrapper.removeChild(hoursWrapper.firstChild);
     }
+
+    //adding event listeners to click events
 document.addEventListener("click", (event)=>{
 if(event.target.matches("#edit")){
-    console.log('edit button clicked');
+enableEditing(userObject);
 } else {
     return false;
 }
 })
+function enableEditing(obj){
+    console.log(obj);
+    divEmpty();
+    let userNameContext = document.createElement('p');
+    userNameContext.textContent = 'Username: ';
+    let userNameInput = document.createElement("input");
+    userNameInput.setAttribute("placeholder", obj.userName);
+    userNameInput.setAttribute("name", "username");
+    userNameInput.classList.add('user-input'); 
+    document.getElementById("user-name").append(userNameContext, userNameInput);
 
+    let addressContext = document.createElement('p');
+    addressContext.textContent = 'Address: ';
+    let addressInput = document.createElement("input");
+    addressInput.setAttribute("placeholder", obj.address);
+    addressInput.setAttribute("name", "address");
+    addressInput.classList.add('user-input'); 
+    document.getElementById("address").append(addressContext, addressInput);
+
+    let emailContext = document.createElement('p');
+    emailContext.textContent = 'contact Email: ';
+    let emailInput = document.createElement("input");
+    emailInput.setAttribute("placeholder", obj.contactEmail);
+    emailInput.setAttribute("name", "contactEmail");
+    emailInput.classList.add('user-input'); 
+    document.getElementById("contact-email").append(emailContext, emailInput);
+    
+    let imgContext = document.createElement('p');
+    imgContext.textContent = 'image Url: ';
+    let imgUrlInput = document.createElement("input");
+    imgUrlInput.setAttribute("placeholder", obj.imageUrl);
+    imgUrlInput.setAttribute("name", "imgUrl");
+    imgUrlInput.classList.add('user-input'); 
+    document.getElementById("img-url").append(imgContext, imgUrlInput);
+
+    let summaryContext = document.createElement("p");
+    summaryContext.textContent = 'Summary: ';
+    let summaryInput = document.createElement("textarea");
+    summaryInput.setAttribute('name', 'summary');
+    summaryInput.setAttribute('maxlength', '500');
+    summaryInput.classList.add('user-input');
+    summaryInput.textContent = obj.summary;
+    document.getElementById('summary').append(summaryContext, summaryInput);
+
+    let plantContext = document.createElement("p");
+    plantContext.textContent = 'plant preferences: ';
+    let plantInput = document.createElement("textarea");
+    plantInput.setAttribute('name', 'plants');
+    plantInput.setAttribute('maxlength', '500');
+    plantInput.classList.add('user-input');
+    plantInput.textContent = obj.plants;
+    document.getElementById('plants').append(plantContext, plantInput);
+    
+
+    let gardenLabel = document.createElement("label");
+    let gardenContext = document.createElement("span");
+    gardenContext.textContent = "I have a garden";
+    let gardenInput = document.createElement("input");
+    gardenInput.setAttribute("type", "checkbox");
+    gardenInput.setAttribute("name", "hasgarden");
+    gardenInput.classList.add("filled-in");
+    gardenLabel.append(gardenInput, gardenContext);
+    document.getElementById("has-garden").append(gardenLabel);
+
+}
 
 
 
